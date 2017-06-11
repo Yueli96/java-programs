85b50bdeaaca0ab87f962336cf7779b4c6a2b095
Nabila Tajrin Bristy <nabilatajrin@users.noreply.github.com>
2017-06-11 10:32:33 +0600

    Create Vertical_print_pattern.java

--- Vertical_print_pattern.java
+++ Vertical_print_pattern.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class Vertical_print_pattern{  
     
-    public static void main(String[]args){
-        
+    public static void main(String[]args){        
+        //Body starts
         Scanner s = new Scanner(System.in);       
         String st = s.nextLine();
         for(int i = 0; i<st.length(); i++){
             System.out.println(st.charAt(i));
-        } 
-        
+        } //Body ends
     }   
 }
 
