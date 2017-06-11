6dca728155b4866e867217a35f7c2f38a36468aa
Nabila Tajrin Bristy <nabilatajrin@users.noreply.github.com>
2017-06-11 11:50:15 +0600

    Create BufferedReader_test.java

---
+++ BufferedReader_test.java
@@ -0,0 +1,24 @@
+package Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReader_test {
+    
+    public static void main(String[] args) throws IOException {
+        int a;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        a = Integer.parseInt(br.readLine());
+        for(int i = 2; i <= a; i++){
+            if(i == a){
+                System.out.println(a + " is a prime number");
+            } else {
+                if (a % 2 == 0) {
+                    System.out.println(a + " is not a prime number");
+                    break;
+                }
+            }
+        }
+    }   
+}
