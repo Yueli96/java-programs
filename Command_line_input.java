ff786bbc60183e5c0cf9ce2052175f1c14a137af
Nabila Tajrin Bristy <nabilatajrin@users.noreply.github.com>
2017-06-11 10:30:48 +0600

    Create Command_line_input.java

--- Command_line_input.java
+++ Command_line_input.java
@@ -1,8 +1,10 @@
 public class Command_line_input {
 	public static void main (String[]args) {
+		
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
 		System.out.println("Total = " + (x+y));
+		
 	}
 }
 
